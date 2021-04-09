359eeed4044a45cf1bb10af974af30e450777303
Mike Burgess <mike@turbot.com>
2021-04-09 13:18:16 +0100

    More WIP for file outputting

--- constants/args.go
+++ constants/args.go
@@ -24,6 +24,7 @@ const (
 	ArgInstallDir              = "install-dir"
 	ArgSqlFile                 = "sql-file"
 	ArgNoColor                 = "no-color"
+	ArgNoProgress              = "no-progress"
 	ArgOutputDir               = "output-dir"
 )
 
